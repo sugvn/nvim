d6b88ecd7d8673b34ee968759e0183d408a5a257
sugvn <suganraj.1211@gmail.com>
2025-07-26 13:40:40 +0530

    added cpp treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -7,7 +7,7 @@ return {
     config = function ()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-          ensure_installed = { "c", "lua","css","javascript", "html","go"},
+          ensure_installed = { "c","cpp","lua","css","javascript", "html","go"},
           sync_install = false,
           highlight = { enable = true,use_languagetree=true},
           indent = { enable = true },
