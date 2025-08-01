e6bf07fc2580d0f4dcb17b4bb89c9c2d4664f119
sugvn <suganraj.1211@gmail.com>
2025-08-01 15:56:04 +0530

    added catpuccin

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,5 +1,16 @@
 return
 {
     "rebelot/kanagawa.nvim",
-    enabled=true
+    enabled = true,
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     vim.cmd [[colorscheme kanagawa-wave]]
+    --     vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+    --     vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+    --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+    --     vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000" })
+    --     vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+    --     vim.api.nvim_set_hl(0, "VertSplit", { bg = "#000000" })
+    -- end
 }
