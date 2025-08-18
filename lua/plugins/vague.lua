2ce6c0fb764dbde1c98aefa5119da56a4e94a9e5
sugvn <suganraj.1211@gmail.com>
2025-08-18 18:11:19 +0530

    change color pallete

--- lua/plugins/vague.lua
+++ lua/plugins/vague.lua
@@ -1,5 +1,6 @@
 return {
         "vague2k/vague.nvim",
+        enabled=false,
         lazy = false,    -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other plugins
         config = function()
