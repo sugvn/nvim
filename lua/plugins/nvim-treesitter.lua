59a9756afa36cca6f22725817eafbf670cabe6da
sugvn <suganraj.1211@gmail.com>
2025-08-23 15:15:55 +0530

    enabled

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,16 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-          ensure_installed = { "c","cpp","lua","css","javascript", "html","go"},
-          sync_install = false,
-          highlight = { enable = true,use_languagetree=false},
-          indent = { enable = true },
-        })
-    end
- }
+        "nvim-treesitter/nvim-treesitter",
+        enabled=true,
+        event = { "UIEnter" },
+        cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+        build = ":TSUpdate",
+        config = function()
+                local configs = require("nvim-treesitter.configs")
+                configs.setup({
+                        ensure_installed = { "c", "lua", "css", "javascript", "html", "go" },
+                        sync_install = false,
+                        highlight = { enable = true, use_languagetree = false },
+                        indent = { enable = true },
+                })
+        end
+}
