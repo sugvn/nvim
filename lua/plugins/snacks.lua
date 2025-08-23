4cd72150849090f133c0994ecbb3b7a507f7bf48
sugvn <suganraj.1211@gmail.com>
2025-08-23 15:16:22 +0530

    add <leader>ff as keymap

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -37,7 +37,7 @@ return {
         -- find
         -- { "<leader>fb", function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
         -- { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-        { "<leader>f", function() Snacks.picker.smart({cwd="~/",layout={preset="ivy"}}) end,    desc = "Find Files" },
+        { "<leader>ff", function() Snacks.picker.smart({cwd="~/",layout={preset="ivy"}}) end,    desc = "Find Files" },
         -- { "<leader>fg", function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
         -- { "<leader>fp", function() Snacks.picker.projects() end,                                desc = "Projects" },
         -- { "<leader>fr", function() Snacks.picker.recent() end,                                  desc = "Recent" },
